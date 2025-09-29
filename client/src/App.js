1375e1d14057513cc84d29b0d4c875741af62828
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-29 09:13:51 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team Sync & Share</h1>
+      <p>Welcome to the collaborative tool for your team!</p>
+    </div>
+  );
+}
+
+export default App;
